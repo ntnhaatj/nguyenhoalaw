dbc9a083ea55b9b15b812f04525af35c38d260ef
ntnhaatj <ntnhat.d@gmail.com>
2022-04-01 15:37:02 +0700

    minor fix

--- nguyenhoalaw/settings/production.py
+++ nguyenhoalaw/settings/production.py
@@ -15,11 +15,11 @@ ALLOWED_HOSTS = ["*"]
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("HD_DB_NAME", "nguyenhoalaw"),
-        'USER': os.environ.get("HD_DB_USER", "nguyenhoalaw"),
-        'PASSWORD': os.environ.get("HD_DB_PASSWORD", "123456"),
-        'HOST': os.environ.get("HD_DB_HOST"),
-        'PORT': os.environ.get("HD_DB_PORT", "5432"),
+        'NAME': os.environ.get("DB_NAME", "nguyenhoalaw"),
+        'USER': os.environ.get("DB_USER", "nguyenhoalaw"),
+        'PASSWORD': os.environ.get("DB_PASSWORD", "123456"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT", "5432"),
     }
 }
 
