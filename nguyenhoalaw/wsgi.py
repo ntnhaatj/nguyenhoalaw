cedd8f019589eb719e95f7ead1fe400c01d3e91e
ntnhaatj <ntnhat.d@gmail.com>
2022-04-01 14:35:19 +0700

    chore: set env to prodcution

--- nguyenhoalaw/wsgi.py
+++ nguyenhoalaw/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nguyenhoalaw.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nguyenhoalaw.settings.dev")
 
 application = get_wsgi_application()
