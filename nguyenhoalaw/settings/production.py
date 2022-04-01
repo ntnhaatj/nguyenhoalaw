6901642ce770f53610952a4b4b878ee7a85f5cc7
ntnhaatj <ntnhat.d@gmail.com>
2022-04-01 15:23:41 +0700

    chore: update db for prod

--- nguyenhoalaw/settings/production.py
+++ nguyenhoalaw/settings/production.py
@@ -12,6 +12,17 @@ DEBUG = False
 # correct to use deployment env
 ALLOWED_HOSTS = ["*"]
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("HD_DB_NAME", "nguyenhoalaw"),
+        'USER': os.environ.get("HD_DB_USER", "nguyenhoalaw"),
+        'PASSWORD': os.environ.get("HD_DB_PASSWORD", "123456"),
+        'HOST': os.environ.get("HD_DB_HOST"),
+        'PORT': os.environ.get("HD_DB_PORT", "5432"),
+    }
+}
+
 try:
     from .local import *
 except ImportError:
